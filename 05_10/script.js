5e802d229529f9c3dcb0b6562fe20fd15eb86c48
davidbrucetn <davidbrucetn@gmail.com>
2021-02-04 20:20:18 -0600

    Thru chap 5 DOM JS

--- 05_10/script.js
+++ 05_10/script.js
@@ -10,6 +10,7 @@
 
 import Backpack from "./Backpack.js";
 
+
 const everydayPack = new Backpack(
   "Everyday Backpack",
   30,
@@ -22,41 +23,57 @@ const everydayPack = new Backpack(
   "../assets/images/everyday.svg"
 );
 
-const content = `
-  
-    <figure class="backpack__image">
-      <img src=${everydayPack.image} alt="" />
-    </figure>
-    <h1 class="backpack__name">Everyday Backpack</h1>
-    <ul class="backpack__features">
-      <li class="packprop backpack__volume">Volume:<span> ${
-        everydayPack.volume
-      }l</span></li>
-      <li class="packprop backpack__color">Color:<span> ${
-        everydayPack.color
-      }</span></li>
-      <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
-      <li class="packprop backpack__pockets">Number of pockets:<span> ${
-        everydayPack.pocketNum
-      }</span></li>
-      <li class="packprop backpack__strap">Left strap length:<span> ${
-        everydayPack.strapLength.left
-      } inches</span></li>
-      <li class="packprop backpack__strap">Right strap length:<span> ${
-        everydayPack.strapLength.right
-      } inches</span></li>
-      <li class="packprop backpack__lid">Lid status:<span> ${
-        everydayPack.lidOpen
-      }</span></li>
+
+const navBarContent = `
+    <ul class="nav__ul">
+      <li class="nav-item home"><a href="#document-top">Home</li>
+      <li class="nav-item about"><a href="#about">About</li>
+      <li class="nav-item purpose"><a href="#purpose">Purpose</li>
+      <li class="nav-item contact"><a href="#contact">Contact</li>
+      <li class="nav-item course"><a href="#course">Course Footer</li>
     </ul>
-  
 `;
 
+
+
+const content = `
+
+<figure class="backpack__image">
+    <img src=${everydayPack.image} alt="" />
+  </figure>
+  <h1 class="backpack__name">Everyday Backpack</h1>
+  <ul class="backpack__features">
+    <li class="packprop backpack__volume">Volume:<span> ${everydayPack.volume}l</span></li>
+    <li class="packprop backpack__color">Color:<span> ${everydayPack.color}</span></li>
+    <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
+    <li class="packprop backpack__pockets">Number of pockets:<span> ${everydayPack.pocketNum}</span></li>
+    <li class="packprop backpack__strap">Left strap length:<span> ${everydayPack.strapLength.left} inches</span></li>
+    <li class="packprop backpack__strap">Right strap length:<span> ${everydayPack.strapLength.right} inches</span></li>
+    <li class="packprop backpack__lid">Lid status:<span> ${everydayPack.lidOpen}</span></li>
+  </ul>
+
+`;
+
+
+
+const header = document.querySelector("header.siteheader");
 const main = document.querySelector(".maincontent");
+
+const siteNav = document.createElement("nav");
+siteNav.classList.add("nav");
+siteNav.innerHTML = navBarContent
+
+
+document.querySelector(".siteheader").prepend(siteNav)
+
 
 const newArticle = document.createElement("article");
 newArticle.classList.add("backpack");
 newArticle.setAttribute("id", "everyday");
 newArticle.innerHTML = content;
 
+
+
 main.append(newArticle);
+
+
